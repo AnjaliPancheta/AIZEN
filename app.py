efad70e0ebffed5823dd2e7b712edb4af35d5a96
AnjaliPancheta <128923775+AnjaliPancheta@users.noreply.github.com>
2024-07-07 00:24:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ mysql = mysql.connector.connect(
     host=app.config['MYSQL_HOST'],
     user=app.config['MYSQL_USER'],
     password=app.config['MYSQL_PASSWORD'],
-    database=app.config['MYSQL_DB']
+    database=app.config['MYSQL_DB'],
     port=os.environ.get('DB_PORT', 3306),
 )
 
